16b93d6e6c488c1a6576c1e0d09be4da6d1a60c5
Isrrael Gutierrez <jgutierrezrivera@kpmg.com>
2019-09-12 12:08:58 -0400

    first commit

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+// Do not edit directly
+// Generated on Thu, 12 Sep 2019 14:38:29 GMT
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
